0841951628b9ca01fc7046b5ee6bfe31d7380c29
Chad Nedzlek <chadnedz@microsoft.com>
2024-10-19 02:15:49 -0700

    now with a little command line thing

---
+++ sec/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace sec;
+
+internal static class Program
+{
+    static int Main(string[] args)
+    {
+        return new Processor(args).Run();
+    }
+}
+
+public interface ICommand<TState, TParent> : ICommand<TState>
+    where TParent : ICommand<TState>
+{
+}
